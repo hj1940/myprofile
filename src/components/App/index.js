88487724c79b751260c8b1983db85da8b0412dd5
hj1940 <ttlle12@naver.com>
2020-03-10 14:34:14 +0900

    add header object

---
+++ src/components/App/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './index.css'
+
+class App extends Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.scrollFunction);
+  }
+
+  scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("header").style.fontSize = "30px";
+    } else {
+      document.getElementById("header").style.fontSize = "90px";
+    }
+  }
+  render() {
+    return (
+      <div className={"App"}>
+        <div className="App-header" id="header">
+          Header
+        </div>
+        <div className="contents">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
