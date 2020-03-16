e837e794cb2a67e0d2ccc21c2f1d792f5a4000a5
hj1940 <ttlle12@naver.com>
2020-03-16 10:02:38 +0900

    contents layout

--- src/components/column/index.js
+++ src/components/column/index.js
@@ -4,37 +4,39 @@ import './index.css'
 class Column extends Component {
   render() {
     return (
-      <div className="leftcolumn">
-        <div className="card">
-          <h2>TITLE HEADING</h2>
-          <h5>Title description, Dec 7, 2017</h5>
-          <div className="fakeimg">Image</div>
-          <p>Some text..</p>
+      <div>
+        <div className="leftcolumn">
+          <div className="card">
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Dec 7, 2017</h5>
+            <div className="fakeimg">Image</div>
+            <p>Some text..</p>
+          </div>
+          <div className="card">
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Sep 2, 2017</h5>
+            <div className="fakeimg">Image</div>
+            <p>Some text..</p>
+          </div>
         </div>
-        <div className="card">
-          <h2>TITLE HEADING</h2>
-          <h5>Title description, Sep 2, 2017</h5>
-          <div className="fakeimg">Image</div>
-          <p>Some text..</p>
-        </div>
+        <div className="rightcolumn">
+          <div className="card">
+            <h2>About Me</h2>
+            <div className="fakeimg">Image</div>
+            <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+          </div>
+          <div className="card">
+            <h3>Popular Post</h3>
+            <div className="fakeimg">Image</div><br/>
+            <div className="fakeimg">Image</div><br/>
+            <div className="fakeimg">Image</div>
+          </div>
+          <div className="card">
+            <h3>Follow Me</h3>
+            <p>Some text..</p>
+          </div>
       </div>
-      <div className="rightcolumn">
-        <div className="card">
-          <h2>About Me</h2>
-          <div class="fakeimg">Image</div>
-          <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-        </div>
-        <div className="card">
-          <h3>Popular Post</h3>
-          <div className="fakeimg">Image</div><br>
-          <div className="fakeimg">Image</div><br>
-          <div className="fakeimg">Image</div>
-        </div>
-        <div className="card">
-          <h3>Follow Me</h3>
-          <p>Some text..</p>
-        </div>
-      </div>
+    </div>
     );
   }
 }
